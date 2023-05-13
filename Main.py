4edb59e5861bd354bf1effb51cd6d6d387f17112
Marsh <marshrey@umich.edu>
2023-05-12 17:21:18 -1000

    it working and has sorting
    Fixed a lot and it outputs nicely.
    Has the ability for additional sorting possibilities. Using online connectivity is not working yet

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import requests
 import math
 import os
+from dataclasses import dataclass
+
+@dataclass
+class Information:
+    name: str
+    playlist_length: float
+    most_popular_song: str
 
 # make a text file for output
 output_file = open('output.txt', 'w')
@@ -44,29 +51,51 @@ user_ID = 'hreynoldsbae' # holly
 #user_ID = current_user_info['id']
 
 # get user playlists
-user_playlists_info = requests.get(BASE_URL + 'users/' + user_ID + '/playlists', headers=headers).json()
-number_of_playlists = user_playlists_info['total']
+user_playlists_info = requests.get(BASE_URL + 'users/' + user_ID + '/playlists?offset=0&limit=1', headers=headers).json()
 
-for i in user_playlists_info['items']: # for each playlist
-    output_file.write('playlist: ' + i['name'] + '\n') # write playlist name to file
+output_info = [] # list of all playlist info
+
+while user_playlists_info: # while there are more playlists to get (has 1 playlist per call)
+    new_playlist_info = Information(user_playlists_info['items'][0]['name'], 0, '') # create new playlist info object
+    print(user_playlists_info['items'][0]['name']) # debugging
     total_playlist_length = 0
-    most_popular_song = [] # list of most popular songs in playlist
+    most_popular_song = '' # most popular song in playlist
     song_popularity = 0 # popularity of most popular song
     # make a call using href to get playlist info
-    single_playlist_info = requests.get(i['tracks']['href'], headers=headers).json()
+    single_playlist_info = requests.get(user_playlists_info['items'][0]['tracks']['href'], headers=headers).json()
     # for each playlist, get all tracks and determine their average length
-    for j in single_playlist_info['items']: # for each track in playlist
-        total_playlist_length += j['track']['duration_ms'] # get track length in ms
-        if j['track']['popularity'] > song_popularity or song_popularity:
-            most_popular_song.clear() # clear list of most popular songs
-            most_popular_song = [j['track']['name']] # add song to list of most popular songs
-            song_popularity = j['track']['popularity'] # set song popularity to current song's popularity
-        elif j['track']['popularity'] == song_popularity:
-            most_popular_song.append(j['track']['name']) # add song to list of most popular songs
         
-    average_playlist_length = round(((total_playlist_length/1000) / single_playlist_info['total'])/60, 2) # get average length in minutes
-    out = math.modf(average_playlist_length)
-    output_file.write('average length: ' + str(round(out[1], 1)) + ' minutes and ' + str(round(out[0] * 60, 2)) + ' seconds' + '\n') # write average length to file
-    output_file.write('most popular song(s): ' + str(most_popular_song) + '\n\n') # write most popular song(s) to file
+    while single_playlist_info: # while there are more tracks to get
+        for i in single_playlist_info['items']: # for each track in the playlist (has 100 tracks per call)
+            if i['track'] == None: # some old tracks have this issue
+                continue
+            total_playlist_length += (i['track']['duration_ms'])/1000 # get track length from ms to s
+            if i['track']['popularity'] > song_popularity:
+                most_popular_song = i['track']['name'] # add song to list of most popular songs
+                song_popularity = i['track']['popularity'] # set song popularity to current song's popularity
+        if single_playlist_info['next'] == None:
+                break
+        single_playlist_info = requests.get(single_playlist_info['next'], headers=headers).json() # get next batch of tracks
+            
+    new_playlist_info.most_popular_song = most_popular_song
+    new_playlist_info.playlist_length = total_playlist_length/single_playlist_info['total']/60
+    print(new_playlist_info.playlist_length) # debugging
+    
+    # add playlist info to list
+    output_info.append(new_playlist_info)
+        
+    if user_playlists_info['next'] == None:
+        break
+    user_playlists_info = requests.get(user_playlists_info['next'], headers=headers).json() # get next batch of playlists
+    
+# sort list by playlist length
+output_info.sort(key=lambda x: x.playlist_length, reverse=True)  
+    
+# write to output file
+for i in output_info:
+    output_file.write(i.name + '\n') # write playlist name to file
+    out = math.modf(i.playlist_length)
+    output_file.write('length: ' + str(round(out[1], 1)) + ' minutes and ' + str(round(out[0] * 60, 2)) + ' seconds' + '\n') # write playlist length to file
+    output_file.write('most popular song: ' + str(i.most_popular_song) + '\n\n') # write most popular song to file
     
 output_file.close()
